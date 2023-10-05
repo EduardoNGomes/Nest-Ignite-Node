a85d8a06247f4a68247d93e9827194bbbffa7d99
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-10-05 17:36:03 -0300

    Controller de autenticação

--- src/controllers/authenticate.controller.ts
+++ src/controllers/authenticate.controller.ts
@@ -1,18 +1,18 @@
 import {
   Body,
-  ConflictException,
   Controller,
   HttpCode,
   Post,
+  UnauthorizedException,
   UsePipes,
 } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
+import { compare } from 'bcryptjs'
 import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
 import { PrismaService } from 'src/prisma/prisma.service'
 import { z } from 'zod'
 
 const authenticateBodySchema = z.object({
-  name: z.string(),
   email: z.string().email(),
   password: z.string(),
 })
@@ -32,16 +32,22 @@ export class AuthenticateController {
   async handle(@Body() body: AuthenticateBodySchema) {
     const { email, password } = body
 
-    const userWithSameEmail = await this.prisma.user.findUnique({
+    const user = await this.prisma.user.findUnique({
       where: { email },
     })
 
-    if (userWithSameEmail) {
-      throw new ConflictException('User with same email already exists')
+    if (!user) {
+      throw new UnauthorizedException('User credentials do not match')
     }
 
-    const token = this.jwt.sign({ sub: 'user-id' })
+    const isPasswordValid = await compare(password, user.password)
 
-    return token
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('User credentials do not match')
+    }
+
+    const accessToken = this.jwt.sign({ sub: user.id })
+
+    return { access_token: accessToken }
   }
 }
