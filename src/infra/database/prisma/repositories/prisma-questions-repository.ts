bef310fc5cb5a8d119e12d633fd3351391ae3662
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-10-06 16:39:07 -0300

    Implementando QuestionsRepository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -19,23 +19,52 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     return PrismaQuestionMapper.toDomain(question)
   }
 
-  findBySlug(slug: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  async findBySlug(slug: string) {
+    const question = await this.prisma.question.findUnique({
+      where: { slug },
+    })
+
+    if (!question) return null
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
-  findManyRecent(params: PaginationParams): Promise<Question[]> {
-    throw new Error('Method not implemented.')
+  async findManyRecent(params: PaginationParams) {
+    const questions = await this.prisma.question.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (params.page - 1) * 20,
+    })
+
+    return questions.map((question) => PrismaQuestionMapper.toDomain(question))
   }
 
-  save(question: Question): Promise<void> {
-    throw new Error('Method not implemented.')
+  async create(question: Question) {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.create({ data })
   }
 
-  create(question: Question): Promise<void> {
-    throw new Error('Method not implemented.')
+  async save(question: Question) {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
   }
 
-  delete(question: Question): Promise<void> {
-    throw new Error('Method not implemented.')
+  async delete(question: Question) {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.delete({
+      where: {
+        id: data.id,
+      },
+    })
   }
 }
