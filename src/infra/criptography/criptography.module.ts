e5f8064eb21f86c69682817e163028fb3d8061d6
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-10-09 09:42:17 -0300

    Implementacao de criptografia

---
+++ src/infra/criptography/criptography.module.ts
@@ -0,0 +1,26 @@
+import { Encrypter } from '@/domain/forum/application/criptography/encrypter'
+import { Module } from '@nestjs/common'
+import { JwtEncrypter } from './jwt-encrypter'
+import { HashComparer } from '@/domain/forum/application/criptography/hash-comparer'
+import { BcryptHasher } from './bcrypt-hasher'
+import { HashGenerator } from '@/domain/forum/application/criptography/hash-generator'
+
+@Module({
+  providers: [
+    {
+      provide: Encrypter,
+      useClass: JwtEncrypter,
+    },
+    {
+      provide: HashComparer,
+      useClass: BcryptHasher,
+    },
+    {
+      provide: HashGenerator,
+      useClass: BcryptHasher,
+    },
+  ],
+
+  exports: [Encrypter, HashComparer, HashGenerator],
+})
+export class CryptographyModule {}
