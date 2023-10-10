1afc6bce1ef334dd8608329b0c7580b115361127
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-10-10 18:06:55 -0300

    Controller upload de arquivo

---
+++ src/infra/http/controllers/upload-attachment.controller.e2e-spec.ts
@@ -0,0 +1,40 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Upload attachment', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    jwt = moduleRef.get(JwtService)
+
+    studentFactory = moduleRef.get(StudentFactory)
+
+    await app.init()
+  })
+  test('[POST] /attachments', async () => {
+    const user = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: user.id.toString() })
+
+    const response = await request(app.getHttpServer())
+      .post(`/attachments`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .attach('file', './test/e2e/curriculum.pdf')
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
---
+++ src/infra/http/controllers/upload-attachment.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
+import { FileInterceptor } from '@nestjs/platform-express'
+
+@Controller('/attachments')
+export class UploadAttachmentController {
+  // constructor(private readonly ) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async handle(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({
+            maxSize: 1024 * 1024 * 2, // 2 mb
+          }),
+          new FileTypeValidator({ fileType: '.(png|jpg|jpeg|pdf)' }),
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
+    console.log(file)
+  }
+}
