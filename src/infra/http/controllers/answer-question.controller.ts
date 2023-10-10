a41e44bd989f7a939b125619d688f3f75e8399d1
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-10-10 09:48:47 -0300

    Controller: Responder Pergunta

---
+++ src/infra/http/controllers/answer-question.controller.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
+
+const answerQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+const bodyValidationPipe = new ZodValidationPipe(answerQuestionBodySchema)
+type AnswerQuestionBodySchema = z.infer<typeof answerQuestionBodySchema>
+
+@Controller('/questions/:questionId/answers')
+export class AnswerQuestionController {
+  constructor(private readonly answerQuestion: AnswerQuestionUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @Body(bodyValidationPipe) body: AnswerQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.answerQuestion.execute({
+      content,
+      questionId,
+      authorId: userId,
+      attachmentsIds: [],
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
